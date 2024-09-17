f29fb21196c274d26fa08d186642a38681f665f2
just_flash_kit <mhoanga1@gmail.com>
2024-09-18 02:03:29 +0700

    improve ProfileAdapter and UserProfile when click user when using search

--- app/src/main/java/cdio/desert_eagle/project_bts/listener/OnProfileItemListener.java
+++ app/src/main/java/cdio/desert_eagle/project_bts/listener/OnProfileItemListener.java
@@ -3,5 +3,9 @@ package cdio.desert_eagle.project_bts.listener;
 public interface OnProfileItemListener {
     void option(Long postId);
 
+    void like(long userId, long postId, int position);
+
+    void liked(long userId, long postId, int position);
+
     void comment(Long postId);
 }
