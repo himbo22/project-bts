40eafa9c1a2b61016c5b25d5957f6d46c808667c
just_flash_kit <mhoanga1@gmail.com>
2024-09-20 00:36:57 +0700

    complete message screen

--- app/src/main/java/cdio/desert_eagle/project_bts/MessageActivity.java
+++ app/src/main/java/cdio/desert_eagle/project_bts/MessageActivity.java
@@ -1,13 +1,22 @@
 package cdio.desert_eagle.project_bts;
 
+import static cdio.desert_eagle.project_bts.constant.ConstantList.BASE_URL;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.bumptech.glide.Glide;
+
+import java.util.Objects;
+
 import cdio.desert_eagle.project_bts.adapter.MessageAdapter;
 import cdio.desert_eagle.project_bts.databinding.ActivityMessageBinding;
+import cdio.desert_eagle.project_bts.model.request.Message;
+import cdio.desert_eagle.project_bts.model.request.UserMessage;
 import cdio.desert_eagle.project_bts.viewmodel.MessageViewModel;
 
 public class MessageActivity extends AppCompatActivity {
@@ -16,6 +25,8 @@ public class MessageActivity extends AppCompatActivity {
     MessageViewModel messageViewModel;
     MessageAdapter messageAdapter;
     Intent intent;
+    String receiverAvatar;
+    Long receiverId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +38,16 @@ public class MessageActivity extends AppCompatActivity {
         intent = getIntent();
         messageViewModel = new MessageViewModel(this.getApplication());
         messageAdapter = new MessageAdapter(
-                intent.getStringExtra("userAvatar"),
+                messageViewModel.userAvatar,
                 intent.getStringExtra("receiverAvatar"));
+        receiverAvatar = intent.getStringExtra("receiverAvatar");
+        receiverId = intent.getLongExtra("receiverId", 0L);
 
         binding.rvMessage.setLayoutManager(new LinearLayoutManager(this));
         binding.rvMessage.setAdapter(messageAdapter);
-
+        binding.tvUsername.setText(intent.getStringExtra("username"));
+        Glide.with(this).load(BASE_URL + "/api/images/" + intent.getStringExtra("receiverAvatar"))
+                .into(binding.imgAvatar);
 
 //        binding.btn.setOnClickListener(v -> {
 //            userListViewModel.sendMessage(
@@ -51,10 +66,36 @@ public class MessageActivity extends AppCompatActivity {
 //                new Message(7L, "", "hoang lon")
 //        );
 
+        binding.btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Objects.requireNonNull(binding.etMessage.getText()).toString().isBlank()) {
+                    return;
+                }
+                messageViewModel.sendMessage(
+                        new UserMessage(intent.getStringExtra("username"), receiverId, receiverAvatar),
+                        new Message(binding.etMessage.getText().toString())
+                );
+                binding.etMessage.setText("");
+            }
+        });
+
+        binding.imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         messageViewModel.getMessage(intent.getLongExtra("receiverId", 0L));
 
         messageViewModel.messagesLiveData.observe(this, messages -> {
             messageAdapter.setChatMessages(messages);
+            binding.rvMessage.smoothScrollToPosition(messageAdapter.getItemCount());
+        });
+
+        messageViewModel.realTimeMessageLiveData.observe(this, message -> {
+            binding.rvMessage.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
         });
 
     }
