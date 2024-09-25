3dec023eacc7a3f398d193230e2189b05c1e0c30
twicelike <doquochuy16905@gmail.com>
2024-09-25 21:20:51 +0700

    feature home screen

--- app/src/main/java/cdio/desert_eagle/project_bts/listener/OnProfileItemListener.java
+++ app/src/main/java/cdio/desert_eagle/project_bts/listener/OnProfileItemListener.java
@@ -3,9 +3,5 @@ package cdio.desert_eagle.project_bts.listener;
 public interface OnProfileItemListener {
     void option(Long postId);
 
-    void like(long userId, long postId, int position);
-
-    void liked(long userId, long postId, int position);
-
     void comment(Long postId);
 }
--- app/src/main/java/cdio/desert_eagle/project_bts/view/fragment/HomeFragment.java
+++ app/src/main/java/cdio/desert_eagle/project_bts/view/fragment/HomeFragment.java
@@ -2,23 +2,36 @@ package cdio.desert_eagle.project_bts.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import cdio.desert_eagle.project_bts.R;
 
 public class HomeFragment extends Fragment {
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button button = view.findViewById(R.id.Add_status);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PostFragment postFragment = new PostFragment();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.hide(HomeFragment.this);
+                transaction.replace(R.id.home_screen, postFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        return view;
     }
 }
